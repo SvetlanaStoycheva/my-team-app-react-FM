ce34b13ad924bd74bb87742fbcd81c527bb20d2d
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-12-08 18:15:22 +0200

    navbar, sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
+import picture from '../assets/bg-pattern-about-1-mobile-nav-1.svg';
+//src/assets/bg-pattern-about-1-mobile-nav-1.svg
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    isSidebarOpen ? setSidebarOpen(false) : setSidebarOpen(true);
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setSidebarOpen(false);
   };
   return (
     <>
@@ -18,11 +23,11 @@ const Navbar = () => {
               <img className='logo-img' src={logo} alt='logo' />
             </Link>
           </div>
-          <button type='button' className='nav-toggle' onClick={toggleSidebar}>
-            {isSidebarOpen ? <FaTimes /> : <FaBars />}
+          <button type='button' className='nav-toggle' onClick={openSidebar}>
+            {<FaBars />}
           </button>
         </div>
-        <div>
+        <div className='nav-links-container'>
           <ul className='nav-links'>
             <li className='nav-link'>
               <Link className='single-nav-link' to='./'>
@@ -36,33 +41,47 @@ const Navbar = () => {
             </li>
           </ul>
           <div className='nav-btn-container'>
-            <button>
-              <Link to='/contact'>contact us</Link>
+            <button className='btn'>
+              <Link className='nav-contact-btn-link' to='/contact'>
+                contact us
+              </Link>
             </button>
           </div>
         </div>
       </nav>
       {/* sidebar */}
-      <aside
-        className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}
-      >
-        <ul className='sidebar-links'>
-          <li className='nav-link'>
-            <Link className='single-nav-link' to='./'>
-              home
-            </Link>
-          </li>
-          <li className='nav-link'>
-            <Link className='single-nav-link' to='./about'>
-              about
-            </Link>
-          </li>
-        </ul>
-        <button className='btn' onClick={() => setSidebarOpen(false)}>
-          <Link className='btn' to='/contact'>
-            contact us
-          </Link>
-        </button>
+      <aside className='sidebar-container'>
+        <div
+          className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}
+        >
+          <div className='sidebar-inner-container'>
+            <button
+              type='button'
+              className='nav-toggle nav-close-btn'
+              onClick={closeSidebar}
+            >
+              {<FaTimes />}
+            </button>
+            <ul className='sidebar-links' onClick={closeSidebar}>
+              <li className='sidebar-link'>
+                <Link className='single-sidebar-link' to='./'>
+                  home
+                </Link>
+              </li>
+              <li className='sidebar-link'>
+                <Link className='single-sidebar-link' to='./about'>
+                  about
+                </Link>
+              </li>
+            </ul>
+            <button className='btn' onClick={() => setSidebarOpen(false)}>
+              <Link className='sidebar-contact-btn' to='/contact'>
+                contact us
+              </Link>
+            </button>
+            <img className='sidebar-img' src={picture} alt='picture' />
+          </div>
+        </div>
       </aside>
     </>
   );
