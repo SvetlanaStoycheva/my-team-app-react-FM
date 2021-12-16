77112de765e81794618349dbf25c79d58cb78e21
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-12-16 17:43:07 +0200

    about page completed

---
+++ src/components/ClientsAboutPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import img from '../assets/bg-pattern-about-4.svg';
+import logo1 from '../assets/logo-gadgets-now.png';
+import logo2 from '../assets/logo-jakarta-post.png';
+import logo3 from '../assets/logo-tech-radar.png';
+import logo4 from '../assets/logo-the-guardian.png';
+import logo5 from '../assets/logo-the-verge.png';
+
+const ClientsAboutPage = () => {
+  return (
+    <section className='some-clients-container'>
+      <img src={img} alt='bg-image' className='bg-image-some-clients' />
+      <h2>Some of our clients</h2>
+      <div className='clients-logo-container'>
+        <img src={logo1} alt='client-logo' className='clients-logo' />
+        <img src={logo2} alt='client-logo' className='clients-logo' />
+        <img src={logo3} alt='client-logo' className='clients-logo' />
+        <img src={logo4} alt='client-logo' className='clients-logo' />
+        <img src={logo5} alt='client-logo' className='clients-logo' />
+      </div>
+    </section>
+  );
+};
+
+export default ClientsAboutPage;
--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeaderAboutPage from '../components/HeaderAboutPage';
 import MeetDirectorsAboutPage from '../components/MeetDirectorsAboutPage';
+import ReadyToStartHomepage from '../components/ReadyToStartHomepage';
+import ClientsAboutPage from '../components/ClientsAboutPage';
 
 const AboutPage = () => {
   return (
     <>
       <HeaderAboutPage />
       <MeetDirectorsAboutPage />
+      <ClientsAboutPage />
+      <ReadyToStartHomepage />
     </>
   );
 };
